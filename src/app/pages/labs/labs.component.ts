471cce406fa6f685653f516668ab67d9c7833d93
Giselle Arroyo <giselle.arroyor@hotmail.com>
2024-05-30 17:53:28 -0600

    Event binding : keydown

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -36,4 +36,9 @@ export class LabsComponent {
   console.log(event)
   }
 
+  keydownHandler(event: KeyboardEvent){
+    const input = event.target as HTMLInputElement;
+    console.log(input.value)
+  }
+
 }
