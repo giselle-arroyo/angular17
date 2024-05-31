b6962dedc6077b7d2f8bbc0d6ae21892612bac0e
Giselle Arroyo <giselle.arroyor@hotmail.com>
2024-05-31 13:27:47 -0600

    Managing forms with validators

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Task } from '../models/task.model'
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -24,10 +25,20 @@ export class HomeComponent {
     },
   ]);
 
-  changeHandler(event : Event){
-    const input=event.target as HTMLInputElement;
-    const newTask=input.value
-    this.addTask(newTask);
+  newTaskControl = new FormControl('',{
+    nonNullable:true,
+    validators:[
+      Validators.required,
+    ]
+  });
+
+  changeHandler(){
+    if(this.newTaskControl.valid){
+      const value=this.newTaskControl.value;
+      this.addTask(value);
+      this.newTaskControl.setValue('');
+    }
+    
   }
 
   addTask(title:string){
